b8cbdd53014c86d00492824d01c0e651f2b3e3af
Flora Afroza <afzflora@gmail.com>
2023-12-17 11:17:52 -0500

    .

--- vocab-trivia.py
+++ vocab-trivia.py
@@ -1,13 +1,13 @@
 # python3 vocab-trivia.py words_alpha.txt
 
 # word text file from https://github.com/dwyl/english-words
-from PyDictionary import PyDictionary
+# from Pydictionary import Dictionary
 import sys
 import random
 import pandas as pd
 
 
-dictionary = PyDictionary()
+#dictionary = PyDictionary()
 
 # Generate random link of certain root word by letter
 wroot_link = 'https://en.m.wikipedia.org/wiki/List_of_Greek_and_Latin_roots_in_English/'
@@ -54,6 +54,7 @@ def extract_root(root_str):     # selects random root from row
 def random_root():
     return extract_root(get_root(root_link(random_letter), ge_row_index(root_link(random_letter))))
 
+print(random_root)
 
 # Generate a word that starts with the root
 
@@ -75,14 +76,11 @@ def find_longest(charseq, min_length):
         return ()
     return find_longest(charseq, min_length - 1)
 
-def generate_rword(root, min_length):   # TODO: watch out if there exists a min length with root word
+def generate_rword(root, min_length):   # watch out if there exists a min length with root word
     words = find_longest(root, min_length)
     return words[random.randrange(len(words))]
 
-lucky_root = random_root();
-print(lucky_root);
-rword = generate_rword(lucky_root, 13)
-print(rword)
+print(generate_rword(random_root, 16))
 
 # Parse the following root afterwards and generate another word that starts with it, if the steps fail then repeat previous step of generating a word that starts with root.
 
@@ -94,10 +92,8 @@ print(rword)
 
 
 # Pick a random word, find definition
-def define_word(lucky_word):    # if PyDictionary doesnt include word, use other dictionaries
-    return dictionary.meaning(lucky_word)
 
-print(define_word(rword))
+
 # Display definition and display 6 words
 def get_root_meaning(root):
     table = tuple(pd.read_html(root_link(random_letter)))
