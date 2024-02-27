412c5fc4f0cf30b660e6deacaf36e42e53e76914
Flora Afroza <afzflora@gmail.com>
2024-02-27 12:19:11 -0500

    ui file added (Dear PyGUI)

---
+++ trivia_gui.py
@@ -0,0 +1,23 @@
+
+import dearpygui.dearpygui as dpg
+from vocab_trivia import *
+
+
+'''
+refer to:
+ 
+https://dearpygui.readthedocs.io/en/latest/documentation/render-loop.html
+'''
+
+
+dpg.create_context()
+
+with dpg.window(tag="Primary Window"):
+    dpg.add_text("Hello, world")
+
+dpg.create_viewport(title='Custom Title', width=600, height=200)
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.set_primary_window("Primary Window", True)
+dpg.start_dearpygui()
+dpg.destroy_context()
