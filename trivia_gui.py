d374def58e198d4db3d4ef5b41857c3c818121c1
Flora Afroza <afzflora@gmail.com>
2024-02-28 04:17:50 -0500

    todo: format buttons

--- trivia_gui.py
+++ trivia_gui.py
@@ -9,11 +9,20 @@ refer to:
 https://dearpygui.readthedocs.io/en/latest/documentation/render-loop.html
 '''
 
+lucky_word = select_word(18)
+lucky_definition = wikitionary_search(lucky_word)
+similar_words = list(words_with_root(other_root(lucky_word), 18, 6))
+root_list = roots_in_word_list(lucky_word)
 
 dpg.create_context()
 
 with dpg.window(tag="Primary Window"):
-    dpg.add_text("Hello, world")
+    dpg.add_text(lucky_definition)
+    
+    for x in range(len(similar_words)):
+        dpg.add_button(label=similar_words[x], callback=lambda x: similar_words[x].trim() == lucky_word.trim(), user_data=similar_words[x])
+    for x in range(len(root_list)):
+        dpg.add_button(label=root_list[x], callback=root_definition, user_data=root_list[x])
 
 dpg.create_viewport(title='Custom Title', width=600, height=200)
 dpg.setup_dearpygui()
